74bb3b58ea7e6085ed8e20c64cdac0e3935d360e
kimbom <kingbom_300432@hotmail.com>
2021-08-28 00:54:07 +0700

    use Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   app.use(compression());
   await app.listen(3000);
 }
